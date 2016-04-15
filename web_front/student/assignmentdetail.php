2621f9d9c6878945821da5bc93ec514d9f3c3631
AugustBeyer <xylaphonies@gmail.com>
2016-04-15 16:49:40 -0700

    removed about from navbar

--- web_front/student/assignmentdetail.php
+++ web_front/student/assignmentdetail.php
@@ -43,9 +43,7 @@ include "assignmentinfo.php";
 <div id="nav">
     <ul class="nav">
         
-        <li><a href="home.php" class="link active" onclick="return false" onmousedown="autoScrollTo('mainPageWrapper');">ASSIGNMENTS</a>
-        </li>
-        <li><a href="home.php" class="link"onclick="return false"onmousedown="autoScrollTo('about');">ABOUT</a>
+        <li><a href="home.php" class="link active" onclick="return false">HOME</a>
         </li>
         <li><a href="settings.php" class="link">SETTINGS</a>
         </li>
