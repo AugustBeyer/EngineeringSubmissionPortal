8cfac8387ba879c68e8f0028efcdfaeeaa8e581e
Joseph Villanueva <javillanueva1@gmail.com>
2016-04-15 22:17:02 -0700

    Edit Assignment Backend

--- web_front/advisor/assignmentinfo.php
+++ web_front/advisor/assignmentinfo.php
@@ -210,13 +210,8 @@ EAHTML */
     //Point Total
     echo "Point Total: *<input type = \"number\" name = \"point_total\"value = \"" . $current_assignment["point_total"] . "\"> <br><br>";
 
-    //Assignment Description
-    echo "Assign to teams: * <br><br>";
-    echo "all teams: <input type=\"checkbox\" onclick=\"checkAll(this)\"><br>";
-    echo "<!--<input type = \"text\" name=\"Teamname\"> -->". printTeamCheckboxes() ."<br><br>";
-
     //Reference File
-    echo "Point Total: *<input class=\"newAssignmentButton\" type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\" value = \"" . $current_assignment["reference_file_name"] . "\"> <br><br>";
+    echo "Reference File: *<input class=\"newAssignmentButton\" type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\" value = \"" . $current_assignment["reference_file_name"] . "\"> <br><br>";
 
     //Description
     echo "Description:*<br>";
