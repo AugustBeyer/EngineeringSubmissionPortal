25de71a5f91ed0205ab38e5c16834056a32185e5
Jonathan Sofer <jdsofer@gmail.com>
2016-04-24 23:28:50 -0700

    Added notification skeleton. No links to these pages yet.

---
+++ web_front/student/studentNotifications.php
@@ -0,0 +1,44 @@
+<?php
+if (!isset($_SESSION))
+    session_start();
+if (!(isset($_SESSION["user_id"]) && $_SESSION["isAdvisor"] == 0))
+{
+    die("Unauthorized access. Please return to the login page.");
+} 
+require "../../backend/db_config.php";
+
+function displayStudentNotifcations()
+{
+	//Echo this out
+	<ul>
+        <li  id="notif1">
+            <div class="sampleNotification">
+                <p class="bodyStyle">Sample notification title</p>
+                <p class="bodyDetail">Sample detail</p>
+            </div>
+            <div class="sampleNotificationMenu">
+                <p><a class="eraseNotification" onmousedown="removeNotification('#notif1');">x</a></p>
+            </div>
+        </li>   
+        <li  id="notif2">
+            <div class="sampleNotification">
+                <p class="bodyStyle">Sample notification title</p>
+                <p class="bodyDetail">Sample detail</p>
+            </div>
+            <div class="sampleNotificationMenu">
+                <p><a class="eraseNotification" onmousedown="removeNotification('#notif2');">x</a></p>
+            </div>
+        </li>   
+        <li  id="notif3">
+            <div class="sampleNotification">
+                <p class="bodyStyle">Sample notification title</p>
+                <p class="bodyDetail">Sample detail</p>
+            </div>
+            <div class="sampleNotificationMenu">
+                <p><a class="eraseNotification" onmousedown="removeNotification('#notif3');">x</a></p>
+            </div>
+        </li>   
+    </ul>  
+}
+
+?> 
