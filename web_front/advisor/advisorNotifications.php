4c5038723c3f97502c99f7e166ffd0ec450612aa
Jonathan Sofer <jdsofer@gmail.com>
2016-04-29 23:02:00 -0700

    added notifications to advisor home page

--- web_front/advisor/advisorNotifications.php
+++ web_front/advisor/advisorNotifications.php
@@ -1,44 +1,52 @@
 <?php
 if (!isset($_SESSION))
     session_start();
-if (!(isset($_SESSION["user_id"]) && $_SESSION["isAdvisor"] == 0))
+if (!(isset($_SESSION["user_id"]) && $_SESSION["isAdvisor"] == 1))
 {
     die("Unauthorized access. Please return to the login page.");
 } 
 require "../../backend/db_config.php";
 
+$advisor_id = $_SESSION["user_id"];
+$notifications = array();
+
+try 
+{
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //Get all notifications for student, ordered by creation date
+    $stmt = $dbh->prepare("SELECT * FROM notifications WHERE notification_advisor_id = :notification_advisor_id");
+    $stmt->bindParam(':notification_student_id', $student_id);
+    $stmt->execute();
+    $notifications = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
+catch(PDOException $e)
+{
+    echo "Error: " . $e->getMessage();
+}
+
+$dbh = null;
+
 function displayAdvisorNotifcations()
 {
-	//Echo out here
-	<ul>
-        <li  id="notif1">
-            <div class="sampleNotification">
-                <p class="bodyStyle">Sample notification title</p>
-                <p class="bodyDetail">Sample detail</p>
-            </div>
-            <div class="sampleNotificationMenu">
-                <p><a class="eraseNotification" onmousedown="removeNotification('#notif1');">x</a></p>
-            </div>
-        </li>   
-        <li  id="notif2">
-            <div class="sampleNotification">
-                <p class="bodyStyle">Sample notification title</p>
-                <p class="bodyDetail">Sample detail</p>
-            </div>
-            <div class="sampleNotificationMenu">
-                <p><a class="eraseNotification" onmousedown="removeNotification('#notif2');">x</a></p>
-            </div>
-        </li>   
-        <li  id="notif3">
-            <div class="sampleNotification">
-                <p class="bodyStyle">Sample notification title</p>
-                <p class="bodyDetail">Sample detail</p>
-            </div>
-            <div class="sampleNotificationMenu">
-                <p><a class="eraseNotification" onmousedown="removeNotification('#notif3');">x</a></p>
-            </div>
-        </li>   
-    </ul>
+    global $notifications;
+    echo "<ul>";
+    for ($i = 0; $i < count($notifications); $i++)
+    {
+        echo "<li id=\"notif\"" . $notifications[$i]["notification_id"] . ">";
+        echo "<div class=\"sampleNotification\">";
+            echo "<a href=\"" . $notifications[$i]["notification_hyperlink"] . "\" >";
+            echo "<p class=\"bodyStyle\">" . $notifications[$i]["notification_title"] . "</p>";
+            echo "<p class=\"bodyDetail\">" . $notifications[$i]["notification_text"] . "</p>";
+            echo "</a>";
+        echo "</div>";
+        echo "<div class=\"sampleNotificationMenu\">";
+            echo "<p><a class=\"eraseNotification\" onmousedown=\"removeNotification('#notif" . $notifications[$i]["notification_id"] . "');\">x</a></p>";
+        echo "</div>";
+        echo "</li>";
+    }  
+    echo "</ul>";  
 }
 
 ?> 
--- web_front/advisor/home.php
+++ web_front/advisor/home.php
@@ -6,6 +6,7 @@ if (!(isset($_SESSION["user_id"]) && $_SESSION["isAdvisor"] == 1))
 } 
 include "teaminfo.php";
 include "assignmentinfo.php";
+include "advisorNotifications.php"
 ?>
 
 <html lang="en">
@@ -92,35 +93,7 @@ include "assignmentinfo.php";
     <br><br><br><br>
         <p class="titleStyle">Notifications</p>
     <hr>
-    <ul>
-        <li  id="notif1">
-            <div class="sampleNotification">
-                <p class="bodyStyle">Sample notification title</p>
-                <p class="bodyDetail">Sample detail</p>
-            </div>
-            <div class="sampleNotificationMenu">
-                <p><a class="eraseNotification" onmousedown="removeNotification('#notif1');">x</a></p>
-            </div>
-        </li>   
-        <li  id="notif2">
-            <div class="sampleNotification">
-                <p class="bodyStyle">Sample notification title</p>
-                <p class="bodyDetail">Sample detail</p>
-            </div>
-            <div class="sampleNotificationMenu">
-                <p><a class="eraseNotification" onmousedown="removeNotification('#notif2');">x</a></p>
-            </div>
-        </li>   
-        <li  id="notif3">
-            <div class="sampleNotification">
-                <p class="bodyStyle">Sample notification title</p>
-                <p class="bodyDetail">Sample detail</p>
-            </div>
-            <div class="sampleNotificationMenu">
-                <p><a class="eraseNotification" onmousedown="removeNotification('#notif3');">x</a></p>
-            </div>
-        </li>   
-    </ul>  
+    <?php displayAdvisorNotifcations(); ?> 
     </div>
     </body>
 </html>
