078c60ad180e67459f18b708ee8e0e3dc4fb41b5
AugustBeyer <xylaphonies@gmail.com>
2016-04-30 20:30:30 -0700

    debugging

--- web_front/student/studentNotifications.php
+++ web_front/student/studentNotifications.php
@@ -10,6 +10,7 @@ require "../../backend/db_config.php";
 $student_id = $_SESSION["user_id"];
 $notifications = array();
 $notifications_hyperlink_array = array();
+$assignments_id_results = array();
 
 try 
 {
@@ -20,18 +21,17 @@ try
     $stmt->bindParam(':notification_student_id', $student_id);
     $stmt->execute();
     $notifications = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    print_r($notifications[$i]["notification_assignment_id"]);
-    $notification_assignment_id = $notifications[$i]["notification_assignment_id"];
+
         
-    //Get all assignments for a team using assignment_team_id
-    $stmt = $dbh->prepare("SELECT primary_assignment_id FROM assignments WHERE assignment_team_id = :team_id");
-    $stmt->bindParam(':team_id', $notification_assignment_id);
-    $stmt -> execute();
-    $assignments_id_results = array();
-    $assignments_id_results = $stmt->fetchAll(PDO::FETCH_COLUMN);
-    print_r($assignments_id_results);
+
     for ($i = 0; $i < count($notifications); $i++)
     {
+        $notification_assignment_id = $notifications[$i]["notification_assignment_id"];
+        //Get all assignments for a team using assignment_team_id
+        $stmt = $dbh->prepare("SELECT primary_assignment_id FROM assignments WHERE assignment_team_id = :team_id");
+        $stmt->bindParam(':team_id', $notification_assignment_id);
+        $stmt -> execute();
+        $assignments_id_results = $stmt->fetchAll(PDO::FETCH_COLUMN);
         //print_r($assignments_id_results);
         for($j = 0; $j < count($assignments_id_results); $j++)
         {
