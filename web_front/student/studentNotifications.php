302a21d62617f23356b9c03c6e96c105a51b08c5
AugustBeyer <xylaphonies@gmail.com>
2016-04-30 20:34:25 -0700

    debug

--- web_front/student/studentNotifications.php
+++ web_front/student/studentNotifications.php
@@ -32,15 +32,15 @@ try
         $stmt->bindParam(':team_id', $notification_assignment_id);
         $stmt -> execute();
         $assignments_id_results = $stmt->fetchAll(PDO::FETCH_COLUMN);
-        //print_r($assignments_id_results);
+        print_r($assignments_id_results);
+        echo($notification_assignment_id);
         for($j = 0; $j < count($assignments_id_results); $j++)
-        {
+        {   
             if ($assignments_id_results[$j] == $notification_assignment_id)
             {
                 $notification_index = $j;
                 $notification_hyperlink = "assignmentdetail.php?table_index=" . $notification_index;
                 array_push($notifications_hyperlink_array, $notification_hyperlink);
-                echo("success");
                 break;
             }
         }
