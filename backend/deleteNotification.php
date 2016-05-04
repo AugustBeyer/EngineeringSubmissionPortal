6054d6a47deb241c01db4ed571fadd3f2a822d7d
Jonathan Sofer <jdsofer@gmail.com>
2016-05-03 22:59:47 -0700

    added backend delete functionality for notifications

--- backend/deleteNotification.php
+++ backend/deleteNotification.php
@@ -3,8 +3,27 @@ session_start();
 
 require "db_config.php";
 
-$notification_id = $_POST["notif_id"];
+$full_notification_id = $_POST["notif_id"];
 
-echo $notification_id;
+//Get rid of initial notif string, e.g., notif23 should just be 23
+$notification_id = filter_var($full_notification_id, FILTER_SANITIZE_NUMBER_INT);
+
+//echo $notification_id;
+
+try {
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //Delete entry from teams
+    $stmt = $dbh->prepare("DELETE FROM notifications WHERE notification_id = :notification_id");
+    $stmt->bindParam(':notification_id', $notification_id);
+    $stmt->execute();
+
+    }
+catch(PDOException $e)
+    {
+    echo "Error: " . $e->getMessage();
+    }
+
+$dbh = null;
 
 ?>
