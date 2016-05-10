b405291f018707cbf218d2fe020fa30e34f1ae28
Jonathan Sofer <jdsofer@gmail.com>
2016-05-09 21:10:17 -0700

    edit form change

--- web_front/advisor/teaminfo.php
+++ web_front/advisor/teaminfo.php
@@ -161,16 +161,16 @@ function editTeamForm($i)
     echo "<div id =\"appendHerePlease\">";
     echo "Team Name: <input class=\"form_field\" type =\"text\" name =\"Teamname\" value=\"" . $project_name_result . "\"><br><br>";
 
-    for ($j=0; $j < count($students_name_results); $j++) 
+    for ($j=0; $j < count($students_name_results); $++j) 
     { 
-        echo "Student " . $j+1;
+        echo "Student " . $j;
         echo " Name: " . $students_name_results[$j] . " <input type=\"checkbox\" id=\"myCheck" . $i ."\" name=\"oldstudents[]\" value=\"" . $students_name_results[$j] ."\" checked><br>";
         echo "<input type=\"hidden\" id=\"myCheck" . $i ."\" name=\"oldstudents[]\" value=\"" . $students_name_results[$j] ."\"><br>";
     }
     echo "</div>";
 
     echo "<div id =\"appendAdvisorsHere\">";
-    for ($j=0; $j < count($advisors_id_results); $j++) 
+    for ($j=0; $j < count($advisors_id_results); $++j) 
     { 
         $stmt = $dbh->prepare("SELECT advisor_username FROM advisors WHERE primary_advisor_id = :primary_advisor_id");
         $stmt->bindParam(':primary_advisor_id', $advisors_id_results[$j]);
@@ -178,7 +178,7 @@ function editTeamForm($i)
         $advisor_name_result = $stmt->fetch(PDO::FETCH_COLUMN);
        // print_r($advisor_name_result);
 
-        echo "Advisor " . $j+1 . " Name: " . $advisor_name_result . " <input type=\"checkbox\" id=\"myCheck" . $i ."\" name=\"oldadvisors[]\" value=\"" . $advisor_name_result ."\" checked><br>";
+        echo "Advisor " . $j. " Name: " . $advisor_name_result . " <input type=\"checkbox\" id=\"myCheck" . $i ."\" name=\"oldadvisors[]\" value=\"" . $advisor_name_result ."\" checked><br>";
         echo "<input type=\"hidden\" id=\"myCheck" . $i ."\" name=\"oldadvisors[]\" value=\"" . $advisor_name_result . "\"><br>";
     }
     echo "</div>";
